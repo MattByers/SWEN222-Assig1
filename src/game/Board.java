497362391cdb465e93c30d3f8ad8fb16c0ce54de
ItsNotAGoodTime <ItsNotAGoodTime@192.168.2.2>
2015-08-01 19:00:55 +1200

    cbf

--- src/game/Board.java
+++ src/game/Board.java
@@ -2,13 +2,10 @@ package game;
 
 import game.rooms.*;
 import game.squares.*;
-
 import java.io.File;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Board {

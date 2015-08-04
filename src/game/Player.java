98e716a6d029ff61c3ac13527cd356b0eca9c36f
Matt Byers <matt_byers@live.com>
2015-08-04 21:43:38 +1200

    accusation fixes

--- src/game/Player.java
+++ src/game/Player.java
@@ -58,8 +58,14 @@ public class Player {
 	public void giveCard (Card card){
 		this.hand.add(card);
 	}
-
-	public boolean takeTurn(Scanner input) {
+	
+	/**
+	 * Allows the player to take a turn, switching over all their possible actions (actions they are allowed dependent on their scenario)
+	 * until they end their turn. Uses scanner as its method for user input.
+	 * 
+	 * @param Scanner input
+	 */
+	public void takeTurn(Scanner input) {
 		this.input = input;
 		this.finished = false;
 		this.possibleActions = new HashSet<String>();
@@ -129,11 +135,14 @@ public class Player {
 				break;
 			}
 		}
-		return retired;
 	}
 	
+	/**
+	 * Shows the current envelope to the user, method only used for debug purposes, wont be shown to a user unless enabled prior.
+	 */
+	
 	private void showEnvelope() {
-		System.out.println("WARNING: About to show your envelope, you are either Matt, Jashon or cheating!\n");
+		System.out.println("WARNING: About to show the game's envelope, you are either Matt, Jashon or cheating!\n");
 		
 		try {
 			Thread.sleep(3000);
@@ -147,19 +156,35 @@ public class Player {
 		System.out.println("");
 		
 	}
-
+	/**
+	 * Random number between 2-12 (i.e game uses two die).
+	 */
 	private void rollDice(){
-		this.diceRoll = (int)(Math.random() * 6) + (int)(Math.random() * 6);
+		this.diceRoll = (int)(Math.random() * 5) + (int)(Math.random() * 5) + 2;
 		this.diceRoll = 20; //DEBUG line
 		System.out.println("You rolled a " + this.diceRoll + "!\n");
 	}
 	
+	/**
+	 * Asks user input to find out which door they would like to leave through, uses numbers to interface with the user, each door is 
+	 * assigned a number from 1 to n.doors
+	 */
 	private void leaveRoom() {
-		System.out.println("Which door would you like to leave through? (1-n)");
-		for(int i = 0; i < this.room.getDoorList().size(); i++){
-			System.out.printf("{%c : %d}", this.room.getDoorList().get(i).getID(), i+1);
+		
+		int exitInd = -1;
+		while(exitInd < 0 || exitInd > this.room.getDoorList().size() - 1){
+			System.out.println("Which door would you like to leave through? (1-n)");
+			for(int i = 0; i < this.room.getDoorList().size(); i++){
+				System.out.printf("{%c : %d}", this.room.getDoorList().get(i).getID(), i+1);
+			}
+			try{
+				exitInd = Integer.parseInt(this.input.nextLine()) -1;
+			}catch(NumberFormatException e){
+				exitInd = -1;
+			}
 		}
-		DoorSquare exit = this.room.getDoorList().get(Integer.parseInt(this.input.nextLine()) - 1);
+			
+		DoorSquare exit = this.room.getDoorList().get(exitInd);
 		this.board.leaveRoom(this, exit);
 		this.location = exit;
 		
@@ -172,28 +197,49 @@ public class Player {
 		this.possibleActions.remove(STAIRS);
 	}
 
+	/**
+	 * Switches the current room to the room joined to this one by stairs.
+	 */
 	private void useStairs() {
 		this.room = this.room.getStairs();
 		this.location = this.board.enterRoom(this, this.room);
 		this.board.printToConsole();
 		System.out.printf("You have used the stairs and are now in the %s.\n", this.room.getName());
 	}
-
+	/**
+	 * Asks the user which item and person they are suggesting, then calls the game class to check the suggestion (item, person, currrent room)
+	 * against the hands of the other players to see if they refuse or accept it.
+	 */
 	private void suggestion() {
 		
 		System.out.printf("You are making a suggestion, containing the %s\n", this.room.getName());
 		
-		System.out.println("Who would you like to accuse? Your options are: (1-6)\n");
-		for(int i = 0; i < this.game.getPersonCards().size(); i++){
-			System.out.printf("{%s : %d}\n", this.game.getPersonCards().get(i).getName(), i+1);
+		int personInd = -1;
+		while(personInd < 0 || personInd > 5){
+			System.out.println("Who would you like to accuse? Your options are: (1-6)\n");
+			for(int i = 0; i < this.game.getPersonCards().size(); i++){
+				System.out.printf("{%s : %d}\n", this.game.getPersonCards().get(i).getName(), i+1);
+			}
+			try{
+				personInd = Integer.parseInt(this.input.nextLine()) -1;
+			}catch(NumberFormatException e){
+				personInd = -1;
+			}
 		}
-		int personInd = Integer.parseInt(this.input.nextLine()) -1;
 
-		System.out.println("\nWith which item?? Your options are: (1-6)\n");
-		for(int i = 0; i < this.game.getItemCards().size(); i++){
-			System.out.printf("{%s : %d}\n", this.game.getItemCards().get(i).getName(), i+1);
+		
+		int itemInd = -1;
+		while(itemInd < 0 || itemInd > 5){
+			System.out.println("\nWith which item?? Your options are: (1-6)\n");
+			for(int i = 0; i < this.game.getItemCards().size(); i++){
+				System.out.printf("{%s : %d}\n", this.game.getItemCards().get(i).getName(), i+1);
+			}
+			try{
+				itemInd = Integer.parseInt(this.input.nextLine()) -1;
+			}catch(NumberFormatException e){
+				itemInd = -1;
+			}
 		}
-		int itemInd = Integer.parseInt(this.input.nextLine()) -1;
 		
 		PersonCard person = this.game.getPersonCards().get(personInd);
 		ItemCard item = this.game.getItemCards().get(itemInd);
@@ -210,31 +256,56 @@ public class Player {
 		this.possibleActions.remove(SUGGEST);
 		
 	}
-
+	/**
+	 * Like suggestion, but asks for a room as well. It also causes you to lose or win the game depending on the outcome.
+	 */
 	private void accusation() {
 		ArrayList<Card> envelope = this.game.getEnvelope();
 		
 		System.out.println("\nAre you sure you want to make an accusation?");
-		System.out.println("If you are wrong, you will be out of the game! (Y or N)");
-		if(!this.input.nextLine().equals("Y")) return;
+		System.out.println("If you are wrong, you will be out of the game! (y to accept, else any key)");
+		if(!this.input.nextLine().equals("y")) return;
 		
-		System.out.println("\nWho would you like to accuse? Your options are: (1-6)\n");
-		for(int i = 0; i < this.game.getPersonCards().size(); i++){
-			System.out.printf("{%s : %d}\n", this.game.getPersonCards().get(i).getName(), i+1);
+		
+		int personInd = -1;
+		while(personInd < 0 || personInd > 5){
+			System.out.println("\nWho would you like to accuse? Your options are: (1-6)\n");
+			for(int i = 0; i < this.game.getPersonCards().size(); i++){
+				System.out.printf("{%s : %d}\n", this.game.getPersonCards().get(i).getName(), i+1);
+			}
+			try{
+				personInd = Integer.parseInt(this.input.nextLine()) -1;
+			}catch(NumberFormatException e){
+				personInd = -1;
+			}
 		}
-		int personInd = Integer.parseInt(this.input.nextLine()) -1;
 
-		System.out.println("\nWith which item?? Your options are: (1-6)\n");
-		for(int i = 0; i < this.game.getItemCards().size(); i++){
-			System.out.printf("{%s : %d}\n", this.game.getItemCards().get(i).getName(), i+1);
+		int itemInd = -1;
+		while(itemInd < 0 || itemInd > 5){
+			System.out.println("\nWith which item?? Your options are: (1-6)\n");
+			for(int i = 0; i < this.game.getItemCards().size(); i++){
+				System.out.printf("{%s : %d}\n", this.game.getItemCards().get(i).getName(), i+1);
+			}
+			try{
+				itemInd = Integer.parseInt(this.input.nextLine()) -1;
+			}catch(NumberFormatException e){
+				itemInd = -1;
+			}
 		}
-		int itemInd = Integer.parseInt(this.input.nextLine()) -1;
 
-		System.out.println("\nIn which room??? Your options are: (1-9)\n");
-		for(int i = 0; i < this.game.getRoomCards().size(); i++){
-			System.out.printf("{%s : %d}\n", this.game.getRoomCards().get(i).getName(), i+1);
+		
+		int roomInd = -1;
+		while(roomInd < 0 || roomInd > 8){
+			System.out.println("\nIn which room??? Your options are: (1-9)\n");
+			for(int i = 0; i < this.game.getRoomCards().size(); i++){
+				System.out.printf("{%s : %d}\n", this.game.getRoomCards().get(i).getName(), i+1);
+			}
+			try{
+				roomInd = Integer.parseInt(this.input.nextLine()) -1;
+			}catch(NumberFormatException e){
+				roomInd = -1;
+			}
 		}
-		int roomInd = Integer.parseInt(this.input.nextLine()) -1;
 
 		
 		PersonCard person = this.game.getPersonCards().get(personInd);
@@ -261,7 +332,10 @@ public class Player {
 		}
 		
 	}
-
+	
+	/**
+	 * Shows the hand of the player.
+	 */
 	private void showCards(){
 		
 		System.out.println("WARNING: About to show your cards, other players look away now!\n");
@@ -282,6 +356,10 @@ public class Player {
 		this.finished = true;
 	}
 	
+	/**
+	 * Asks the user to specify a direction,  then checks the validity of the move with board and moves them to the new square if valid. 
+	 * This continues until they are out of dice rolls. Will cause the player to enter a room if they move onto a doorway.
+	 */
 	//THIS METHOD IS BROKE TO FUCK
 	private void movePlayer(){
 		while(this.diceRoll > 0){
